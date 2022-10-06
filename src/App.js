24c75444b368b5e5ebc3a0cb2622a15ec1a32525
DaeHyun Lee <dhlee2@autocrypt.io>
2022-10-06 17:51:51 +0900

    미니블로그 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import MainPage from "./components/page/MainPage";
+import PostWritePage from "./components/page/PostWritePage";
+import PostViewPage from "./components/page/PostViewPage";
+import logo from "./logo.svg";
+import "./App.css";
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainTitleText>미니 블로그 연습</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />}></Route>
+        <Route path="post-write" element={<PostWritePage />}></Route>
+        <Route path="post/:postId" element={<PostViewPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
